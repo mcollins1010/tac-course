1f604507da70c3fb9399f4ff91b0e39009c5e5ea
kunj3770@gmail.com <mbuton70@gmail.com>
2024-03-24 11:48:21 -0400

    updated vpc

--- lesson-08/part-A/user2-config/main.tf
+++ lesson-08/part-A/user2-config/main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "user2-vpc" {
 module "webserver-user2" {
   source         = "../modules/webserver"
   ami            = "ami-0b8b44ec9a8f90422"
-  vpc_id         = aws_vpc.user2-vpc
+  vpc_id         = aws_vpc.user2-vpc.id
   instance_type  = "t2.micro"
   webserver_name = "User2's"
   cidr_block     = "10.0.0.0/24"
