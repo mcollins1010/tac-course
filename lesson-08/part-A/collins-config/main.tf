cdd9fabfa02064a939de23e8dff69fef47839f96
kunj3770@gmail.com <mbuton70@gmail.com>
2024-03-24 10:29:21 -0400

    Added Modules and 1 user env

---
+++ lesson-08/part-A/collins-config/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+module "webserver" {
+  source         = "../modules/webserver"
+  ami            = "ami-0b8b44ec9a8f90422"
+  vpc_id         = aws_vpc.my_vpc.id
+  instance_type  = "t2-micro"
+  webserver_name = "Collins'"
+  cidr_block     = "10.0.0.0/16"
+}
---
+++ lesson-08/part-A/modules/webserver/variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "CIDR_BLOCK"
+}
+
+variable "ami" {
+  type        = string
+  description = "AMI"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Instance type"
+}
+
+variable "webserver_name" {
+  type        = string
+  description = "serner name"
+}
+
