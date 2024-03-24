141653226a36c584acbcad7e4e93922bc0f5a76b
kunj3770@gmail.com <mbuton70@gmail.com>
2024-03-24 11:42:51 -0400

    updated ami

--- lesson-08/part-A/user2-config/main.tf
+++ lesson-08/part-A/user2-config/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "user2-vpc" {
 
 module "webserver-user2" {
   source         = "../modules/webserver"
-  ami            = "ami-0d77c9d87c7e619f9"
+  ami            = "ami-0b8b44ec9a8f90422"
   vpc_id         = aws_vpc.my_vpc.id
   instance_type  = "t2.micro"
   webserver_name = "User2's"
