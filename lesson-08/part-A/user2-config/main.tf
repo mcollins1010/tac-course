323401f5d72e5c8ee593c8daece3b18bc52ea322
kunj3770@gmail.com <mbuton70@gmail.com>
2024-03-24 11:15:44 -0400

    added user2 env

---
+++ lesson-08/part-A/user2-config/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "user2-vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+module "webserver-user2" {
+    source         = "../modules/webserver"
+  ami            = "ami-0b8b44ec9a8f90422"
+  vpc_id         = aws_vpc.my_vpc.id
+  instance_type  = "t2.micro"
+  webserver_name = "User2's"
+  cidr_block     = "10.0.0.0/16"
+}
