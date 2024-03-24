3a0de921522417648ea0d7427bca52e854ff96dc
kunj3770@gmail.com <mbuton70@gmail.com>
2024-03-24 11:29:10 -0400

    created redShift server

--- lesson-08/part-A/user2-config/main.tf
+++ lesson-08/part-A/user2-config/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 resource "aws_vpc" "user2-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/24"
 }
 
 module "webserver-user2" {
   source         = "../modules/webserver"
-  ami            = "ami-0b8b44ec9a8f90422"
+  ami            = "ami-0d77c9d87c7e619f9"
   vpc_id         = aws_vpc.my_vpc.id
   instance_type  = "t2.micro"
   webserver_name = "User2's"
-  cidr_block     = "10.0.0.0/16"
+  cidr_block     = aws_vpc.user2-vpc
 }
